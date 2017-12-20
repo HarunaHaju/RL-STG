04efc94f390545ec5b20836e6e1a424d3c24b485
harunahaju <lwx9688@foxmail.com>
2017-12-20 22:54:08 +0800

    Update 12/20

--- src/main/java/cn/lwx/rlstg/interfaces/StepPerFrame.java
+++ src/main/java/cn/lwx/rlstg/interfaces/StepPerFrame.java
@@ -5,12 +5,12 @@ package cn.lwx.rlstg.interfaces;
  * Comments:
  * Author: lwx
  * Create Date: 2017/12/18
- * Modified Date: 2017/12/18
+ * Modified Date: 2017/12/20
  * Why & What is modified:
  * Version: 0.0.1beta
  * It's the only NEET thing to do. – Shionji Yuuko
  */
 
 public interface StepPerFrame {
-    public void step();
+    void step();
 }
