246073a5b2832ae4b3fb971a4b98feed6a542290
harunahaju <lwx9688@foxmail.com>
2017-12-20 22:02:35 +0800

    init

---
+++ src/main/java/cn/lwx/rlstg/GlobalManager.java
@@ -0,0 +1,15 @@
+package cn.lwx.rlstg;
+
+/**
+ * Package: cn.lwx.rlstg
+ * Comments:
+ * Author: lwx
+ * Create Date: 2017/12/20
+ * Modified Date: 2017/12/20
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class GlobalManager {
+
+}
