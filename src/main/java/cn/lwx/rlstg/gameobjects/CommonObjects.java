ca451f37071af347f29233f22e0e6d75f9d8e136
harunahaju <lwx9688@foxmail.com>
2017-12-20 22:29:56 +0800

    Update 12/20

--- src/main/java/cn/lwx/rlstg/gameobjects/CommonObjects.java
+++ src/main/java/cn/lwx/rlstg/gameobjects/CommonObjects.java
@@ -7,7 +7,7 @@ import cn.lwx.rlstg.interfaces.StepPerFrame;
  * Comments:
  * Author: lwx
  * Create Date: 2017/12/18
- * Modified Date: 2017/12/18
+ * Modified Date: 2017/12/20
  * Why & What is modified:
  * Version: 0.0.1beta
  * It's the only NEET thing to do. – Shionji Yuuko
