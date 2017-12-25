c6ed1ea051516d617a3dbef2baed3118aec2c526
harunahaju <lwx9688@foxmail.com>
2017-12-25 16:18:55 +0800

    Update 12/25 Add shot function

--- src/main/java/cn/lwx/rlstg/gameobjects/Enemy.java
+++ src/main/java/cn/lwx/rlstg/gameobjects/Enemy.java
@@ -1,5 +1,7 @@
 package cn.lwx.rlstg.gameobjects;
 
+import cn.lwx.rlstg.GlobalManager;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.util.Random;
@@ -9,7 +11,7 @@ import java.util.Random;
  * Comments:
  * Author: lwx
  * Create Date: 2017/12/18
- * Modified Date: 2017/12/22
+ * Modified Date: 2017/12/25
  * Why & What is modified:
  * Version: 0.0.1beta
  * It's the only NEET thing to do. – Shionji Yuuko
@@ -46,6 +48,12 @@ public class Enemy extends CommonObjects {
 
     public BufferedImage getImage() {
         return image;
+    }
+
+    public void shot(){
+        Bullet bullet = new Bullet(this.getCenterX(),this.getY()
+                ,this.getSpeed(),this.getDamage(),Bullet.PARENTS_ENEMY);
+        GlobalManager.GLOBAL_MANAGER.getBullets().add(bullet);
     }
 
     @Override
