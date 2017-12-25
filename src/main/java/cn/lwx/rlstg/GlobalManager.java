d2895394f3249efcf6ed902a4898031c15b95cb1
harunahaju <lwx9688@foxmail.com>
2017-12-25 15:45:36 +0800

    Update 12/25 Change the data structure from ArrayList to ConcurrentLinkedQueue

--- src/main/java/cn/lwx/rlstg/GlobalManager.java
+++ src/main/java/cn/lwx/rlstg/GlobalManager.java
@@ -1,18 +1,20 @@
 package cn.lwx.rlstg;
 
+import cn.lwx.rlstg.gameobjects.Bullet;
 import cn.lwx.rlstg.gameobjects.Enemy;
 import cn.lwx.rlstg.gameobjects.Player;
 import cn.lwx.rlstg.interfaces.StepPerFrame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Package: cn.lwx.rlstg
  * Comments:
  * Author: lwx
  * Create Date: 2017/12/20
- * Modified Date: 2017/12/23
+ * Modified Date: 2017/12/25
  * Why & What is modified:
  * Version: 0.0.1beta
  * It's the only NEET thing to do. – Shionji Yuuko
@@ -22,7 +24,8 @@ public class GlobalManager implements StepPerFrame {
     private int liveScore;
 
     private Player player;
-    private ArrayList<Enemy> enemies;
+    private ConcurrentLinkedQueue<Enemy> enemies;
+    private ConcurrentLinkedQueue<Bullet> bullets;
 
     public static final GlobalManager GLOBAL_MANAGER = new GlobalManager();
 
@@ -30,7 +33,8 @@ public class GlobalManager implements StepPerFrame {
         killScore = 0;
         liveScore = 0;
         player = new Player();
-        enemies = new ArrayList<>();
+        enemies = new ConcurrentLinkedQueue<>();
+        bullets = new ConcurrentLinkedQueue<>();
     }
 
     public void reset(){
@@ -63,14 +67,19 @@ public class GlobalManager implements StepPerFrame {
         this.player = player;
     }
 
-    public ArrayList<Enemy> getEnemies() {
+    public ConcurrentLinkedQueue<Enemy> getEnemies() {
         return enemies;
+    }
+
+    public ConcurrentLinkedQueue<Bullet> getBullets() {
+        return bullets;
     }
 
     @Override
     public void step() {
         liveScore++;
         player.step();
-        enemies.forEach(enemy -> step());
+        enemies.forEach(Enemy::step);
+        bullets.forEach(Bullet::step);
     }
 }
