18619dc92a88ac0e04c48626a7afd98ab045f2f9
harunahaju <lwx9688@foxmail.com>
2018-01-11 22:36:09 +0800

    Update 18/01/11 enemy generate randomly

--- src/main/java/cn/lwx/rlstg/gameobjects/Enemy.java
+++ src/main/java/cn/lwx/rlstg/gameobjects/Enemy.java
@@ -11,7 +11,7 @@ import java.util.Random;
  * Comments:
  * Author: lwx
  * Create Date: 2017/12/18
- * Modified Date: 2018/01/01
+ * Modified Date: 2018/01/11
  * Why & What is modified:
  * Version: 0.0.1beta
  * It's the only NEET thing to do. – Shionji Yuuko
@@ -21,7 +21,8 @@ public class Enemy extends CommonObjects {
     private BufferedImage image;
 
     public Enemy(){
-        super(100,50,50,5,10);
+        //CommonObjects(double hp, int x, int y, int speed, double damage)
+        super(100,(int)(Math.random()*300),50,5,10);
         try {
             image = ImageIO.read(Enemy.class.getResource("/img/enemy.png"));
             this.setWidth(image.getWidth());
