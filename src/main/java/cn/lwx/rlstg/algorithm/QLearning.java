ea0375dc58c4b240160d9385044b91d4ac2775a4
harunahaju <lwx9688@foxmail.com>
2018-01-15 22:34:38 +0800

    init

---
+++ src/main/java/cn/lwx/rlstg/algorithm/QLearning.java
@@ -0,0 +1,14 @@
+package cn.lwx.rlstg.algorithm;
+
+/**
+ * Package: cn.lwx.rlstg.algorithm
+ * Comments:
+ * Author: lwx
+ * Create Date: 2018/1/15
+ * Modified Date: 2018/1/15
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class QLearning {
+}
