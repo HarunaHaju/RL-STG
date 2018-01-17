27c1d7ee7bf08d448ca67a931cf15793b38cce7d
harunahaju <lwx9688@foxmail.com>
2018-01-17 08:45:35 +0800

    init

--- src/main/java/cn/lwx/rlstg/algorithm/QLearning/QLearning.java
+++ src/main/java/cn/lwx/rlstg/algorithm/QLearning/QLearning.java
@@ -1,14 +1,19 @@
-package cn.lwx.rlstg.algorithm;
+package cn.lwx.rlstg.algorithm.QLearning;
+
+
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 /**
  * Package: cn.lwx.rlstg.algorithm
  * Comments:
  * Author: lwx
- * Create Date: 2018/1/15
- * Modified Date: 2018/1/15
+ * Create Date: 2018/1/17
+ * Modified Date: 2018/1/17
  * Why & What is modified:
  * Version: 0.0.1beta
  * It's the only NEET thing to do. – Shionji Yuuko
  */
 public class QLearning {
+
 }
---
+++ src/main/java/cn/lwx/rlstg/algorithm/QLearning/QState.java
@@ -0,0 +1,15 @@
+package cn.lwx.rlstg.algorithm.QLearning;
+
+/**
+ * Package: cn.lwx.rlstg.algorithm.QLearning
+ * Comments:
+ * Author: lwx
+ * Create Date: 2018/1/17
+ * Modified Date: 2018/1/17
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class QState {
+
+}
