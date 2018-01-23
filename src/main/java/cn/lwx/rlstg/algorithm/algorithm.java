b7427a90e557a9e2964568b7ee9d716cffd6d4fe
harunahaju <lwx9688@foxmail.com>
2018-01-23 23:08:09 +0800

    init

---
+++ src/main/java/cn/lwx/rlstg/algorithm/algorithm.java
@@ -0,0 +1,15 @@
+package cn.lwx.rlstg.algorithm;
+
+/**
+ * Package: cn.lwx.rlstg.algorithm
+ * Comments:
+ * Author: lwx
+ * Create Date: 2018/1/23
+ * Modified Date: 2018/1/23
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public abstract class algorithm {
+
+}
