e5d1768b1d87224620e8812ae555b92734764689
harunahaju <lwx9688@foxmail.com>
2018-01-25 16:07:24 +0800

    init

---
+++ src/main/java/cn/lwx/rlstg/algorithm/RuleBased/LiveFirst.java
@@ -0,0 +1,24 @@
+package cn.lwx.rlstg.algorithm.RuleBased;
+
+import cn.lwx.rlstg.algorithm.Controller;
+
+/**
+ * Package: cn.lwx.rlstg.algorithm.RuleBased
+ * Comments: This algorithm aims to live longer
+ * Author: lwx
+ * Create Date: 2018/1/24
+ * Modified Date: 2018/1/24
+ * Why & What is modified:
+ * Version: 1.0.0
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class LiveFirst extends Controller {
+    public LiveFirst(){
+        super(Controller.ALGORITHM_LIVEFIRST);
+    }
+
+    @Override
+    public int decide() {
+        return 0;
+    }
+}
