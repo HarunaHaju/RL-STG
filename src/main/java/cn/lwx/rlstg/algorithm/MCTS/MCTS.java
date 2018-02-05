819173369d28b2cc27b4a0db1df66c419d9772e8
harunahaju <lwx9688@foxmail.com>
2018-02-05 21:56:51 +0800

    init

---
+++ src/main/java/cn/lwx/rlstg/algorithm/MCTS/MCTS.java
@@ -0,0 +1,25 @@
+package cn.lwx.rlstg.algorithm.MCTS;
+
+import cn.lwx.rlstg.algorithm.Controller;
+
+/**
+ * Package: cn.lwx.rlstg.algorithm.MCTS
+ * Comments:
+ * Author: lwx
+ * Create Date: 2018/2/5
+ * Modified Date: 2018/2/5
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class MCTS extends Controller {
+
+    public MCTS() {
+        super(Controller.ALGORITHM_MCTS);
+    }
+
+    @Override
+    public int decide() {
+        return 0;
+    }
+}
