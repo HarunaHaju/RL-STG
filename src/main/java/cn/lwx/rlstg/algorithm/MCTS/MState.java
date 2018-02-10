e95c683ef4b962298168292ad4a156afbd6f28f5
harunahaju <lwx9688@foxmail.com>
2018-02-10 23:03:43 +0800

    init

---
+++ src/main/java/cn/lwx/rlstg/algorithm/MCTS/MState.java
@@ -0,0 +1,17 @@
+package cn.lwx.rlstg.algorithm.MCTS;
+
+/**
+ * Package: cn.lwx.rlstg.algorithm.MCTS
+ * Comments:
+ * Author: lwx
+ * Create Date: 2018/2/10
+ * Modified Date: 2018/2/10
+ * Why & What is modified:
+ * Version: 0.0.1beta
+ * It's the only NEET thing to do. – Shionji Yuuko
+ */
+public class MState {
+    public MState() {
+    }
+
+}
