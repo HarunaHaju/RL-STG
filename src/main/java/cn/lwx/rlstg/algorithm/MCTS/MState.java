91c29ef817c832350b8cc547ff13313255d6a8a1
harunahaju <lwx9688@foxmail.com>
2018-02-10 23:06:05 +0800

    Update 18/02/10

--- src/main/java/cn/lwx/rlstg/algorithm/MCTS/MState.java
+++ src/main/java/cn/lwx/rlstg/algorithm/MCTS/MState.java
@@ -1,5 +1,9 @@
 package cn.lwx.rlstg.algorithm.MCTS;
 
+import cn.lwx.rlstg.algorithm.Common.Vector2D;
+
+import java.util.ArrayList;
+
 /**
  * Package: cn.lwx.rlstg.algorithm.MCTS
  * Comments:
@@ -11,7 +15,34 @@ package cn.lwx.rlstg.algorithm.MCTS;
  * It's the only NEET thing to do. – Shionji Yuuko
  */
 public class MState {
-    public MState() {
+    private ArrayList<ArrayList<Vector2D>> lists;
+
+    public MState(ArrayList<Vector2D> bulletDeltaVectors, ArrayList<Vector2D> enemyDeltaVectors) {
+        lists = new ArrayList<>();
+        lists.add(bulletDeltaVectors);
+        lists.add(enemyDeltaVectors);
     }
 
+    public MState() {
+        lists = new ArrayList<>();
+    }
+
+    private ArrayList<ArrayList<Vector2D>> getLists() {
+        return lists;
+    }
+
+    @Override
+    public int hashCode() {
+        return lists != null ? lists.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+        if (this == o)
+            return true;
+        MState state = (MState) o;
+        return (lists.equals(state.getLists()));
+    }
 }
