2a63f7685fddd6085d14733724d883b1bbe35121
harunahaju <lwx9688@foxmail.com>
2018-02-11 11:27:40 +0800

    Update 18/02/11 add updateState()

--- src/main/java/cn/lwx/rlstg/algorithm/MCTS/MCTS.java
+++ src/main/java/cn/lwx/rlstg/algorithm/MCTS/MCTS.java
@@ -1,6 +1,12 @@
 package cn.lwx.rlstg.algorithm.MCTS;
 
+import cn.lwx.rlstg.GlobalManager;
+import cn.lwx.rlstg.algorithm.Common.Vector2D;
 import cn.lwx.rlstg.algorithm.Controller;
+import cn.lwx.rlstg.gameobjects.Bullet;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Package: cn.lwx.rlstg.algorithm.MCTS
@@ -14,6 +20,8 @@ import cn.lwx.rlstg.algorithm.Controller;
  */
 public class MCTS extends Controller {
 
+    private MState state;
+
     public MCTS() {
         super(Controller.ALGORITHM_MCTS);
     }
@@ -21,5 +29,21 @@ public class MCTS extends Controller {
     @Override
     public int decide() {
         return 0;
+    }
+
+    public void updateState() {
+        //add all bullets to list and sort by distance
+        ArrayList<Vector2D> bulletVectors = new ArrayList<>();
+        GlobalManager.GLOBAL_MANAGER.getBullets().forEach(bullet -> {
+            if (bullet.getFlag() == Bullet.PARENTS_ENEMY)
+                bulletVectors.add(new Vector2D(GlobalManager.GLOBAL_MANAGER.getPlayer().getX() - bullet.getX(),
+                        GlobalManager.GLOBAL_MANAGER.getPlayer().getY() - bullet.getY()));
+        });
+        Collections.sort(bulletVectors);
+
+        //update state
+        state = new MState(bulletVectors
+                , new Vector2D(GlobalManager.GLOBAL_MANAGER.getPlayer().getX(),
+                GlobalManager.GLOBAL_MANAGER.getPlayer().getY()));
     }
 }
