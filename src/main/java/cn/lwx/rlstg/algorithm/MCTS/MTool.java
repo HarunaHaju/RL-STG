f2d4f079e6eb593ea341736dcc6c923e4d41e715
harunahaju <lwx9688@foxmail.com>
2018-02-11 10:34:37 +0800

    Update 18/02/11

--- src/main/java/cn/lwx/rlstg/algorithm/MCTS/MTool.java
+++ src/main/java/cn/lwx/rlstg/algorithm/MCTS/MTool.java
@@ -18,10 +18,12 @@ import java.util.ArrayList;
  */
 public class MTool {
     private static final int SPEED = GlobalManager.GLOBAL_MANAGER.getPlayer().getSpeed();
+    private static final int BULLET_SPEED = GlobalManager.GLOBAL_MANAGER.getEnemies().peek().getBulletSpeed();
 
     //return next state, null means lose
     public static MState getSimulateResult(MState state, int action) {
-        ArrayList<Vector2D> route = new ArrayList<>();//the route of player;
+        ArrayList<Vector2D> playerRoute = new ArrayList<>();//the route of player;
+        ArrayList<Vector2D> bulletRoute = new ArrayList<>();
         int deltaX = 0;
         int deltaY = 0;
         switch (action) {
@@ -40,19 +42,52 @@ public class MTool {
                 deltaY = -SPEED;
                 break;
         }
+
+        //add positions
         for (int i = 0; i < Player.MOVE_SPAN; i++) {
-            route.add(new Vector2D(GlobalManager.GLOBAL_MANAGER.getPlayer().getX() + i * deltaX
-                    , GlobalManager.GLOBAL_MANAGER.getPlayer().getY() + i * deltaY));
+            int posX = state.getPlayerPosition().getX() + i * deltaX;
+            int posY = state.getPlayerPosition().getY() + i * deltaY;
+
+            if (posX <= 0) {
+                posX = 0;
+            } else if (posX >= 700) {
+                posX = 700;
+            }
+            if (posY <= 0) {
+                posY = 0;
+            } else if (posY > 480) {
+                posY = 480;
+            }
+
+            playerRoute.add(new Vector2D(posX, posY));
+            for (int j = 0; j < state.getBullets().size(); j++) {
+                bulletRoute.add(new Vector2D(state.getBullets().get(i).getX()
+                        , state.getBullets().get(i).getY() + i * BULLET_SPEED));
+            }
         }
 
-        return null;
+        //judge get shot
+        for (int i = 0; i < playerRoute.size(); i++) {
+            for (int j = 0; j < state.getBullets().size(); j++) {
+                if (judgeGetShot(playerRoute.get(i), bulletRoute.get(j)))
+                    return null;
+            }
+        }
+
+        return new MState(bulletRoute, playerRoute.get(Player.MOVE_SPAN - 1));
     }
 
-    public static boolean judgeGetShot(Vector2D player, ArrayList<Vector2D> bullets) {
+    public static boolean judgeGetShot(Vector2D player, Vector2D bullet) {
         boolean isShot = false;
 
-        for (int i = 0; i < bullets.size(); i++) {
-
+        //the judging condition is same as CommonObjects.java
+        if (bullet.getX() + GlobalManager.GLOBAL_MANAGER.getBullets().peek().getWidth()
+                >= player.getX() + GlobalManager.GLOBAL_MANAGER.getPlayer().getWidth() / 4 &&
+                bullet.getX() <= player.getX() + GlobalManager.GLOBAL_MANAGER.getPlayer().getWidth() / 4 * 3 &&
+                bullet.getY() + GlobalManager.GLOBAL_MANAGER.getBullets().peek().getHeight()
+                        >= player.getY() + GlobalManager.GLOBAL_MANAGER.getPlayer().getHeight() / 4 &&
+                bullet.getY() <= player.getY() + GlobalManager.GLOBAL_MANAGER.getPlayer().getHeight() / 4 * 3) {
+            isShot = true;
         }
 
         return isShot;
